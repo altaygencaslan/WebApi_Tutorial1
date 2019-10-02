ad0ed044a61453bd3ffb033cc22c4646f547927d
altaygencaslan <developer.gencaslan@gmail.com>
2019-10-02 13:34:46 +0300

    Get Methods

---
+++ Models/Personel.cs
@@ -0,0 +1,9 @@
+namespace WebApi_Tutorial1.Models
+{
+    public class Personel
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+    }
+}
